cc81f9b875a94a5dbe124d451dc38ff2e7a43891
DilanJT <dicmandilan@gmail.com>
2025-06-02 17:18:56 +0530

    added piping

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rust_shell
+
+- This is a learning project which I followed the tutorial from https://www.joshmcguigan.com/blog/build-your-own-shell-rust/ 
+
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{
     env,
     io::{Write, stdin, stdout},
     path::Path,
-    process::Command,
+    process::{Child, Command, Stdio},
 };
 
 fn main() {
@@ -13,35 +13,64 @@ fn main() {
         stdin().read_line(&mut input).unwrap();
 
         // read_line leaves a trailing newline, which trim removes
-        let mut parts = input.trim().split_whitespace();
-        println!("Parts: {:?}", parts);
-        let command = parts.next().unwrap();
-        println!("Command: {}", command);
+        // let mut parts = input.trim().split_whitespace();
+        // println!("Parts: {:?}", parts);
 
-        let args = parts;
-        println!("Args: {:?}", args);
+        let mut commands = input.trim().split("|").peekable();
+        let mut previous_command = None;
+        // println!("Command: {}", command);
 
-        match command {
-            "cd" => {
-                // default to '/' as new directory if one was not provided
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
+        while let Some(command) = commands.next() {
+            // println!("Args: {:?}", args);
+
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    // default to '/' as new directory if one was not provided
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+                }
+                "exit" => {
+                    return;
+                }
+                command => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        },
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    }
+
                 }
             }
-            command => {
-                let child = Command::new(command).args(args).spawn();
+        }
 
-                // gracefully handle malformed user input
-                // gracefully handle malformed user input
-                match child {
-                    Ok(mut child) => {
-                        child.wait();
-                    }
-                    Err(e) => eprintln!("{}", e),
-                };
-            }
+        if let Some(mut final_command) = previous_command {
+            final_command.wait();
         }
     }
 }
